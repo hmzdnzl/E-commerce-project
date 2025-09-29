c8948a332fe2f7a64f951f64db98a10b2c1affa3
hmzdnzl <hmzdnzl38@gmail.com>
2025-09-29 20:18:14 +0300

    Proje kurulumu yapıldı

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div id='app' className="flex justify-center items-center  ">
+    
+    
+        
+
+
+      <p className=" text-[50px] text-red-500">
+        E-commerce Website Project will be build here
+      </p>
+    </div>
+  )
+}
+
+export default App
